fbb7726d8ecfe31964bae664c3b5f0ed2be97159
erikggg <erik_grahn@hotmail.com>
2013-11-12 17:51:20 +0100

    done
    done

--- Assignment2/Assignment2/WorkingSchedule.cs
+++ Assignment2/Assignment2/WorkingSchedule.cs
@@ -8,39 +8,66 @@ namespace MAHAssignment2
 {
     class WorkingSchedule
     {
+        public bool done = false;
         public int choice;
         public void Start()
         {
             ListChoices();
-            ShowSchedule();
+            while (!done)
+            {
+
+                ShowSchedule();
+            }
         }
         public void ListChoices()
-        { 
-            Console.WriteLine("1: Show a list of weekends to work");
-            Console.WriteLine("2: Show a list of nights to work");
-            Console.WriteLine("0: Return to main menu");
+        {
+                Console.WriteLine("1: Show a list of weekends to work");
+                Console.WriteLine("2: Show a list of nights to work");
+                Console.WriteLine("0: Return to main menu");
 
-            choice = Input.ReadIntegerConsole();
+                choice = Input.ReadIntegerConsole();
         }
         private void ShowSchedule()
-        { 
-            while (choice != 0)
-            if (choice == 1)
-                ShowWeekendSchedule();
+        {
+            switch (choice)
+            {
+                case 0:
+                    done = true;
+                    break;
+                case 1:
+                    ShowWeekendSchedule();
+                    break;
 
-            if (choice == 2)
-                ShowNightsSchedule();
-            else
-                Console.WriteLine("Invalid menu option");
-        }
+                case 2:
+                    ShowNightsSchedule();
+                    break;
+                    
+                default:
+                    Console.WriteLine("Invalid menu option");
+                    break;
+            }
+        } 
         private void ShowWeekendSchedule()
+        {
+            int week;
+            int maxweek = 52;
+            for (int i = 0; i < maxweek; i += 3)
+            {
+                week = i;
+                if (i == 0)
+                {
+                    week = 6;
+                    i = week;
+                }
+                Console.WriteLine("Week {0}", week);
+            }
+            done = true;
+            return;
+        }
+        private void ShowNightsSchedule()
         {
 
         }
-        private void ShowNightsSchedule()
-        { 
-
-        }
     }
- 
+
 }
