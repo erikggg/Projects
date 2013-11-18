561c5a2bf27547b652a76476af29442dd4ee0f57
erikggg <erik_grahn@hotmail.com>
2013-11-18 17:29:12 +0100

    sent in
    last assignment2 changes before sending in

--- Assignment2/Assignment2/CelciusToFahrenheit.cs
+++ Assignment2/Assignment2/CelciusToFahrenheit.cs
@@ -14,22 +14,34 @@ namespace MAHAssignment2
         public void Start()
         {
             DisplayInfo();
-            DisplayStuff(100);
+            //only showing integers divisible by 4
+            int max = Input.ReadIntegerConsole("Please input the max celsius value", null);
+            int min = Input.ReadIntegerConsole("Please input the minimum celsius value", null);
+            int oldHeight = Console.WindowHeight;
+            DisplayStuff(max, min);
+            Console.SetWindowSize(Console.WindowWidth, oldHeight);
         }
         private void DisplayInfo()
         {
             Console.WriteLine("Temperature Table");
             Console.WriteLine("Showing Celsius (C) to Fahrenheit (F) conversion");
         }
-        private void DisplayStuff(int maxCelsius)
+        //
+        private void DisplayStuff(int maxCelsius, int minCelsius)
         {
-            Console.SetWindowSize(Console.WindowWidth, (maxCelsius/4)+4);
+            //for larger amounts, set window size to amount of lines displayed + 6
+            
+            if ((maxCelsius/4)+6> Console.WindowHeight && (maxCelsius/4)+4 <= 58)
+            {
+                Console.SetWindowSize(Console.WindowWidth, (maxCelsius/4)+6);
+            }
             int valueToConvert;
-            for (int i = 0; i <= maxCelsius; i++)
+            for (int i = minCelsius; i <= maxCelsius; i++)
             {
                 valueToConvert = i;
                 //toCelsius = (5.0 / 9.0 * (valueToConvert - 32));
                 toFahrenheit = (9.0 / 5.0 * valueToConvert + 32.0);
+                //only writing every 4 calculations to allow all calculations to show in the window
                 if (i % 4 == 0)
                 Console.WriteLine("{0, 25} F = {1,8} C", toFahrenheit.ToString("F2"), valueToConvert.ToString("F2"));
             }
