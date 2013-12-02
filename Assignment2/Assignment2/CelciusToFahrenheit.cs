17c85ee4e0dbc083270966a54163d328dfd38ac7
erikggg <erik_grahn@hotmail.com>
2013-12-02 07:49:02 +0100

    removing redundant variable
    There was no need to store the i value in a local variable (afaik)

--- Assignment2/Assignment2/CelciusToFahrenheit.cs
+++ Assignment2/Assignment2/CelciusToFahrenheit.cs
@@ -31,10 +31,11 @@ namespace MAHAssignment2
         }
         private void DisplayInfo()
         {
+            
             Console.WriteLine("Temperature Table");
             Console.WriteLine("Showing Celsius (C) to Fahrenheit (F) conversion");
         }
-        //
+        //taking max and min values to display
         private void DisplayStuff(int maxCelsius, int minCelsius)
         {
             //for larger amounts, set window size to amount of lines displayed + 6
@@ -43,15 +44,14 @@ namespace MAHAssignment2
             {
                 Console.SetWindowSize(Console.WindowWidth, (maxCelsius/4)+6);
             }
-            int valueToConvert;
             for (int i = minCelsius; i <= maxCelsius; i++)
             {
-                valueToConvert = i;
+                
                 //toCelsius = (5.0 / 9.0 * (valueToConvert - 32));
-                toFahrenheit = (9.0 / 5.0 * valueToConvert + 32.0);
+                toFahrenheit = (9.0 / 5.0 * i + 32.0);
                 //only writing every 4 calculations to allow all calculations to show in the window
                 if (i % 4 == 0)
-                Console.WriteLine("{0, 25} F = {1,8} C", toFahrenheit.ToString("F2"), valueToConvert.ToString("F2"));
+                Console.WriteLine("{0, 25} F = {1,8} C", toFahrenheit.ToString("F2"), i.ToString("F2"));
             }
             Console.WriteLine("Press Enter to continue");
             Console.ReadLine();
