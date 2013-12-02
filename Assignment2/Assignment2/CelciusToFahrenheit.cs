440421884a52ac1b80750bc3800660d17ea7bb3d
erikggg <erik_grahn@hotmail.com>
2013-12-02 08:24:03 +0100

    Comments
    Commenting Celcius to Fahrenheit conversion
    further

--- Assignment2/Assignment2/CelciusToFahrenheit.cs
+++ Assignment2/Assignment2/CelciusToFahrenheit.cs
@@ -38,21 +38,27 @@ namespace MAHAssignment2
         //taking max and min values to display
         private void DisplayStuff(int maxCelsius, int minCelsius)
         {
-            //for larger amounts, set window size to amount of lines displayed + 6
-            
-            if ((maxCelsius/4)+6 > Console.WindowHeight && (maxCelsius/4)+4 <= 58)
+            //If rows + 6 is larger than current window height and smaller 
+            //than max window height set window size to 
+            //amount of lines displayed + 6 to display chosen values in same console window
+            //values divided by 4 since we only print every 4th value.
+            if ((maxCelsius/4)+6 > Console.WindowHeight && (maxCelsius/4)+6 <= Console.LargestWindowHeight)
             {
                 Console.SetWindowSize(Console.WindowWidth, (maxCelsius/4)+6);
-            }
+            }            
             for (int i = minCelsius; i <= maxCelsius; i++)
             {
                 
                 //toCelsius = (5.0 / 9.0 * (valueToConvert - 32));
+                //Storing Fahrenheit value in variable to print.
                 toFahrenheit = (9.0 / 5.0 * i + 32.0);
-                //only writing every 4 calculations to allow all calculations to show in the window
+                //only writing every 4 calculations to show in the window
                 if (i % 4 == 0)
+                    //shifting columns slightly to line up nicer
+                    //writing ToString as a float with 2 decimals
                 Console.WriteLine("{0, 25} F = {1,8} C", toFahrenheit.ToString("F2"), i.ToString("F2"));
             }
+            //waiting for command before exiting conversion
             Console.WriteLine("Press Enter to continue");
             Console.ReadLine();
         }
